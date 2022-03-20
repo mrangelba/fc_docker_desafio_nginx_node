7657ad3cf91a6e2b8bdd906770d96a653dc9f9c6
Marcelo <mrangelba@msn.com>
2022-03-20 17:10:54 -0300

    first commit

---
+++ mysql/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE people (
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
---
+++ node/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const faker = require('faker')
+const app = express()
+const port = process.env.APP_PORT || 3000
+const mysql = require('mysql')
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const connection = mysql.createConnection(config)
+
+app.get('/', (req, res) => {
+  const name = faker.name.findName()
+
+  connection.query(`INSERT INTO people (name) VALUES ('${name}')`)
+
+  connection.query(`SELECT name FROM people`, (error, results, fields) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.name}</li>`).join('') : ''}
+      </ol>
+    `)
+  })
+})
+
+app.listen(port, () => {
+  console.log('Rodando na porta:', port);
+})
